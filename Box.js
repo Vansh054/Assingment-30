f536eb34096c4de2b56f7f7b84c5686019d1e255
Vansh054 <72291779+Vansh054@users.noreply.github.com>
2020-11-03 22:35:01 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,40 @@
+class Box
+{
+    constructor(x,y,w,h){
+        var option={
+            isStatic:false
+        }
+     this.body = Bodies.rectangle(x,y,w,h,option) 
+     this.width = w
+     this.height = h
+     this.image = loadImage("chocochip.png")
+     World.add(world,this.body)
+    }
+    display(){
+         if (this.body.speed <= 5)
+        {   
+        var pos = this.body.position
+        var angle = this.body.angle
+        this.maxSpeed = 255
+        push();
+            translate(pos.x,pos.y)
+            rotate(angle)
+            imageMode(CENTER)
+            fill("pink")
+            image(this.image,0,0,this.width,this.height)
+        pop();  
+        } 
+        else 
+        {
+            World.remove(world,this.body)
+            push();
+            tint(255,this.maxSpeed)
+            image(this.image,this.body.position.x,this.body.position.y,this.width,this.height)
+            this.maxSpeed = this.maxSpeed - 30
+            console.log(this.body)
+        pop();
+        }
+    }
+    
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Assingment-30
+# p5.play-boilerplate
+Boiler plate for p5.play
